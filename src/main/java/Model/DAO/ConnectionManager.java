9c4a427cf84ca8641abe9eb02597d690dc1dc8a8
takumi <00092301014@st04.emtech-academy.com>
2022-05-24 15:28:34 +0900

    add UserBean,UserDAO and some *Complete.html

--- src/main/java/Model/DAO/ConnectionManager.java
+++ src/main/java/Model/DAO/ConnectionManager.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-	private static final String URL = "jdbc:mysql://localhost:3306/user_db";
-	private static final String USER = "embexU";
-	private static final String PASSWORD = "embexP";
+	private static final String URL = "jdbc:mysql://localhost:3306/task_db";
+	private static final String USER = "rabbit";
+	private static final String PASSWORD = "turtle";
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		return DriverManager.getConnection(URL, USER, PASSWORD);
---
+++ src/main/java/Model/entity/UserBean.java
@@ -0,0 +1,34 @@
+package Model.entity;
+
+public class UserBean {
+	private String userId;
+	private String password;
+	private String userName;
+	private String updateDate;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+	
+
+}
