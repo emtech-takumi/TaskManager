d9df02564d3dd7cf92fa20b59840f585f14d1a08
takumi <00092301014@st04.emtech-academy.com>
2022-05-25 14:50:08 +0900

    add LoginServlet

--- src/main/java/Controller/LoginServlet.java
+++ src/main/java/Controller/LoginServlet.java
@@ -1,16 +1,21 @@
 package Controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Model.DAO.UserDAO;
+import Model.entity.UserBean;
+
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+@WebServlet("/login-servlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -33,8 +38,26 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		UserDAO dao = new UserDAO();
+		
+		try {
+			UserBean user = dao.select(request.getParameter("userId"));
+		
+			String password = request.getParameter("password");
+			
+			
+			if(password.equals(user.getPassword())) {
+				//認証成功
+				request.getSession().setAttribute("ID", user.getUserId());
+				request.getRequestDispatcher("Menu-Servlet").forward(request, response);
+			}else {
+				//認証失敗
+				request.getRequestDispatcher("login.html").forward(request, response);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 	}
 
 }
